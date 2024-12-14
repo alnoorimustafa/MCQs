25b627868813718f12c7a13d6cfedeee96b98971
alnoorimustafa <alnoorimustafa@gmail.com>
2024-12-14 20:38:27 +0300

    before x icon

--- mcq/src/App.vue
+++ mcq/src/App.vue
@@ -2,7 +2,18 @@
 import { ref } from "vue";
 import BestOfFive from "./components/BestOfFive.vue";
 import Book600 from "./components/Book600.vue";
+
 let page = ref("");
+let selectedImage = ref<string | null>(null); // Track the selected image
+
+const selectImage = (image: string) => {
+  console.log("Selected Image:", image); // Debugging line
+  selectedImage.value = new URL(image, import.meta.url).href; // Use URL constructor for correct path
+};
+
+const cancelSelection = () => {
+  selectedImage.value = null; // Reset the selection
+};
 </script>
 
 <template>
@@ -41,22 +52,50 @@ let page = ref("");
   </div>
   <div class="container">
     <div class="grid mt-2">
-      <div class="center" @click="page = 'bof'">
+      <div
+        class="center"
+        v-if="!selectedImage"
+        @click="selectImage('./assets/bof1.png')"
+      >
         <img src="./assets/bof1.png" alt="" />
       </div>
-      <div class="center" @click="page = 'bof'">
+      <div
+        class="center"
+        v-if="!selectedImage"
+        @click="selectImage('./assets/bof2.png')"
+      >
         <img src="./assets/bof2.png" alt="" />
       </div>
-      <div class="center" @click="page = 'bof'">
+      <div
+        class="center"
+        v-if="!selectedImage"
+        @click="selectImage('./assets/bof3.png')"
+      >
         <img src="./assets/bof3.png" alt="" />
       </div>
-      <div class="center" @click="page = '600'">
+      <div
+        class="center"
+        v-if="!selectedImage"
+        @click="selectImage('./assets/600p1.png')"
+      >
         <img src="./assets/600p1.png" alt="" />
       </div>
-      <div class="center" @click="page = '600'">
+      <div
+        class="center"
+        v-if="!selectedImage"
+        @click="selectImage('./assets/600p2.png')"
+      >
         <img src="./assets/600p2.png" alt="" />
       </div>
-      <div class="center"><img src="./assets/501.png" alt="" /></div>
+      <div class="center" v-if="!selectedImage">
+        <img src="./assets/501.png" alt="" />
+      </div>
+      <div class="center" v-if="selectedImage">
+        <div class="image-container">
+          <img :src="selectedImage" alt="Selected Image" />
+          <button class="cancel-button" @click="cancelSelection">X</button>
+        </div>
+      </div>
     </div>
   </div>
   <div class="hh">
@@ -121,5 +160,21 @@ footer {
 
 .copyright {
   color: #0172ad;
+}
+
+.image-container {
+  position: relative; /* Position relative for absolute positioning of the button */
+}
+
+.cancel-button {
+  position: absolute;
+  top: 5px; /* Adjust as needed */
+  right: 5px; /* Adjust as needed */
+  background: rgb(255, 45, 45);
+  color: rgb(255, 255, 255); /* Change color as needed */
+  font-size: 20px; /* Adjust size as needed */
+  cursor: pointer;
+  border-color: transparent;
+  font-family: sans-serif;
 }
 </style>
