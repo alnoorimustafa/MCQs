6929e7eee642d049a8ad6d6a82a3f3c0f6f44f8b
alnoorimustafa <alnoorimustafa@gmail.com>
2025-01-04 20:58:51 +0300

    pwa implemented

--- mcq/src/main.ts
+++ mcq/src/main.ts
@@ -1,5 +1,14 @@
-import { createApp } from "vue";
-// @ts-ignore
-import App from "./App.vue";
+import { createApp } from "vue"
 
-createApp(App).mount("#app");
+// @ts-ignore
+import App from "./App.vue"
+
+import "./registerServiceWorker"
+
+createApp(App).mount("#app")
+
+if (window.matchMedia("(display-mode: standalone)").matches) {
+  console.log("This is running as PWA standalone.")
+} else {
+  console.log("This is not PWA.")
+}
