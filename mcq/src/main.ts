9dd0c67a34316e652fb87a421d9076935afd3fc4
Mustafa Alnoori <alnoorimustafa@gmail.com>
2025-04-14 21:48:43 +0300

    deleted service worker

--- mcq/src/main.ts
+++ mcq/src/main.ts
@@ -1,14 +1,6 @@
-import { createApp } from "vue"
+import { createApp } from 'vue'
 
 // @ts-ignore
-import App from "./App.vue"
+import App from './App.vue'
 
-import "./registerServiceWorker"
-
-createApp(App).mount("#app")
-
-if (window.matchMedia("(display-mode: standalone)").matches) {
-  console.log("This is running as PWA standalone.")
-} else {
-  console.log("This is not PWA.")
-}
+createApp(App).mount('#app')
